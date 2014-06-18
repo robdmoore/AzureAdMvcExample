88e8cc0b3e5a3822112b38ce5b816ea95219c728
Rob Moore <me@robdmoore.id.au>
2014-06-18 19:28:18 +0800

    Cleaning up extraneous stuff

--- AzureAdMvcExample/App_Start/BundleConfig.cs
+++ AzureAdMvcExample/App_Start/BundleConfig.cs
@@ -1,11 +1,9 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace AzureAdMvcExample
 {
     public class BundleConfig
     {
-        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
@@ -14,8 +12,6 @@ namespace AzureAdMvcExample
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
@@ -27,8 +23,6 @@ namespace AzureAdMvcExample
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
 
-            // Set EnableOptimizations to false for debugging. For more information,
-            // visit http://go.microsoft.com/fwlink/?LinkId=301862
             BundleTable.EnableOptimizations = true;
         }
     }
