3c55318f24455884b6186fe6a3904fdc3418c78a
Rob Moore <me@robdmoore.id.au>
2014-06-26 16:15:43 +0800

    Added configuration for Windows Azure AD authentication

---
+++ AzureAdMvcExample/App_Start/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System.Configuration;
+using Microsoft.Owin.Security.ActiveDirectory;
+using Owin;
+
+namespace AzureAdMvcExample
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseWindowsAzureActiveDirectoryBearerAuthentication(
+                new WindowsAzureActiveDirectoryBearerAuthenticationOptions
+                {
+                    Audience = ConfigurationManager.AppSettings["ida:AudienceUri"],
+                    Tenant = ConfigurationManager.AppSettings["AzureADTenant"]
+                });
+        }
+    }
+}
